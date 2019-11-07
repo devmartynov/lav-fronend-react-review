decd2c8601fd14e2e63c5d5a81c16332656c0c96
LavrL <llembersk@gmail.com>
2019-11-07 10:13:49 +0200

    Properties page: style refactored

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -21,7 +21,23 @@ const routes = [
 ]
 
 function homepage() {
-    return <h2>Welcome to home page</h2>
+    return (
+        <React.Fragment>
+            <h2>Welcome to home page</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Porro, voluptate aliquam. Laborum vero, asperiores alias
+                iure odio expedita cupiditate reiciendis nobis aliquam dolor,
+                ex dolorem ipsa sit iste nihil deleniti? Esse distinctio in,
+                ratione magni beatae quam perspiciatis? Libero tenetur eaque
+                rem sequi voluptas repudiandae nobis? Voluptas neque quis
+                repudiandae error nam est illo harum voluptatem veritatis quos enim,
+                eveniet distinctio dolorum voluptatibus mollitia hic exercitationem
+                beatae dignissimos. Libero doloremque laboriosam minima ut aut numquam,
+                at quas possimus! Asperiores quasi nobis adipisci dignissimos
+                exercitationem nulla eligendi nemo aliquam sed architecto, esse quia
+                iusto cumque explicabo sapiente. Rem cupiditate quasi eveniet.</p>
+        </React.Fragment>
+    )
 }
 
 function logout() {
--- src/components/PropertiesPage/todo-list-item/todo-list-item.js
+++ src/components/PropertiesPage/todo-list-item/todo-list-item.js
@@ -3,14 +3,14 @@ import React from 'react';
 
 const TodoListItem = (props) => {
     return (
-        <div className="title">
+        <React.Fragment>
             <label className="title">{props.label}</label>
             <input type="text"
                 className="textField" />
             <button className="edit" >Edit</button>
             <button className="delete" 
                     onClick={props.onDelete}>Delete</button>
-        </div>
+        </React.Fragment>
     )
 }
 

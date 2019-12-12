a4ffc42499db7f12f0f1f17c191d4b4c9a264b9b
dmartynov <denis.martynov.93@mail.ru>
2019-12-12 08:55:51 +0700

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+    entry: {
+        main: './src/index.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle-[name].[hash].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        cacheDirectory: true,
+                        plugins: [
+                            ['@babel/plugin-proposal-decorators', {legacy: true}],
+                            '@babel/plugin-proposal-class-properties',
+                        ].filter(Boolean),
+                        presets: [
+                            [
+                                "@babel/preset-env",
+                                {
+                                    "targets": {
+                                        "browsers": "last 2 versions"
+                                    },
+                                    "useBuiltIns": 'entry'
+                                }
+                            ],
+                            '@babel/preset-react',
+                        ].filter(Boolean),
+                    }
+                },
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'css-hot-loader',
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.(woff(2)?)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts/',
+                    },
+                }],
+            },
+        ],
+    },
+    devServer: {
+        contentBase: './src',
+        hot: true,
+        watchContentBase: true,
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style.css',
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+        }),
+    ],
+};
